3b4fa8afc54eec757613d8b4b9419de3582bb2b3
Ilshat <lefrostx@gmail.com>
2016-09-25 05:33:20 +0500

    Implemented open file button click handler

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -15,5 +19,19 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_buttonOpenFile_clicked()
 {
+    QString filename = QFileDialog::getOpenFileName();
 
+    if (filename.isEmpty())
+        return;
+
+    QFile file{filename};
+    file.open(QIODevice::ReadOnly);
+
+    if (!file.isOpen())
+    {
+        QMessageBox::warning(nullptr, "Ошибка", "Ошибка чтения файла " + filename);
+        return;
+    }
+
+    ui->fileContent->setPlainText(QTextStream{&file}.readAll());
 }
